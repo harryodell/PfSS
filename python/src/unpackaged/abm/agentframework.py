3281dff118064675614e5dbc8c14b94fb64ad934
Harry Odell <harry.odell93@gmail.com>
2018-09-18 16:57:46 +0100

    added functions

---
+++ python/src/unpackaged/abm/agentframework.py
@@ -0,0 +1,29 @@
+import random
+
+class Agent:
+    
+    def __init__(self):
+        self.x = random.randint(0,99)
+        self.y = random.randint(0,99)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- python/src/unpackaged/abm/model2.py
+++ python/src/unpackaged/abm/model2.py
@@ -1,94 +1,67 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import random
-import math
+import operator
+import matplotlib.pyplot
+import itertools
+import time
+import agentframework
+
+start = time.clock()
+
+# initialize 
+num_of_agents = 10
+num_of_iterations = 10000
+agents = []
 
 
-agents =[]
+# make the agents
+for i in range(num_of_agents):
+    agents.append([random.randint(0,99),random.randint(0,99)])
 
 
-### first agent
-y0 = random.randint(0,100)
-x0 = random.randint(0,100)
+# move the agents in random direction 
+for j in range(num_of_iterations):
+    for i in range(num_of_agents):
 
-# append coordinates to agent list
-agents.append([y0,x0])
-print(agents)
+        if random.random() < 0.5:
+            agents[i][0] = (agents[i][0] + 1) % 100
+        else:
+            agents[i][0] = (agents[i][0] - 1) % 100
 
-# random walk
-if random.random() < 0.5:
-    y0 += 1
-else:
-    y0 -= 1
-    
-if random.random() < 0.5:
-    x0 += 1
-else:
-    x0 -= 1
-
-# second random walk
-if random.random() < 0.5:
-    y0 += 1
-else:
-    y0 -= 1
-    
-if random.random() < 0.5:
-    x0 += 1
-else:
-    x0 -= 1
+        if random.random() < 0.5:
+            agents[i][1] = (agents[i][1] + 1) % 100
+        else:
+            agents[i][1] = (agents[i][1] - 1) % 100
 
 
+# plot agents on grid
+matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.ylim(0, 99)
+for i in range(num_of_agents):
+    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+matplotlib.pyplot.show()
 
 
+# distance function
+def distance_bet(agentA, agentB):
+    return (((agentA[0] - agentB[0])**2) + 
+        ((agentA[1] - agentB[1])**2))**0.5
 
-### second agent
-y1 = random.randint(0,100)
-x1 = random.randint(0,100)
-# random walk
-if random.random() < 0.5:
-    y1 += 1
-else:
-    y1 -= 1
-    
-if random.random() < 0.5:
-    x1 += 1
-else:
-    x1 -= 1
+distance = []
 
-# second random walk
-if random.random() < 0.5:
-    y1 += 1
-else:
-    y1 -= 1
-    
-if random.random() < 0.5:
-    x1 += 1
-else:
-    x1 -= 1
+#for i, j in itertools.combinations(agents, 2):
+#    disty = distance_bet(i, j)
+#    distance.append(distance)
 
+for i in range(len(agents)):
+    for j in range(i + 1, len(agents)):
+        disty = distance_bet(agents[i], agents[j])
+        distance.append(disty)
 
-print(y0, x0)
-print(y1, x1)
+end = time.clock()
+print("time = " + str(end - start))
 
-
-
-
-#### find distance
-
-distance = math.sqrt( (y1-y0)**2 + (x1-x0)**2 )
-print(distance)
-
-
-
-
-
-
-
+a = agentframework.Agent()
+print(a.y, a.x)
 
 
 
