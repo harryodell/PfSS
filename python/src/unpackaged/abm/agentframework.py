a73a337f6be61eb9db38b54070379a067a9b0573
Harry Odell <harry.odell93@gmail.com>
2018-09-19 16:52:40 +0100

    classwork

--- python/src/unpackaged/abm/agentframework.py
+++ python/src/unpackaged/abm/agentframework.py
@@ -1,13 +1,56 @@
 import random
 
-class Agent:
-    
-    def __init__(self):
+class Agent:    
+    def __init__(self, environment):
+        self.y = random.randint(0,99)        
         self.x = random.randint(0,99)
-        self.y = random.randint(0,99)
+        self.environment = environment
+        self.store = 0 
+
+        
+    def move(self):
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 100
+        else:
+            self.x = (self.x - 1) % 100
+
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 100
+        else:
+            self.y = (self.y - 1) % 100        
 
 
+    def eat(self):
+        if self.environment[self.y][self.x] > 10:
+            self.environment[self.y][self.x] -= 10
+            self.store += 10
+        else:
+            self.store += self.environment[self.y][self.x]
+            self.environment[self.y][self.x] = 0    
+    
+    @property
+    def x(self):
+        return self._x
+    
+    @property
+    def y(self):
+        return self._y
+    
+    @x.setter
+    def x(self, value):
+        self._x = value 
+        
+    @y.setter 
+    def y(self, value):
+        self._y = value
+        
+    
 
+#a = Agent()
+#print(a.y, a.x)
+
+#a.move()
+#print(a.y, a.x)
 
 
 
--- python/src/unpackaged/abm/model2.py
+++ python/src/unpackaged/abm/model2.py
@@ -4,68 +4,99 @@ import matplotlib.pyplot
 import itertools
 import time
 import agentframework
+import csv 
+import pandas as pd
 
-start = time.clock()
+#start = time.clock()
+ 
+# read in data 
+environment = []
+with open('in.txt', 'r') as file_for_reading:
+    for row in file_for_reading:
+        rowlist = row.split(',')
+        rowlisty = list(map(int, rowlist))
+#        rowlist = []	
+#        for value in row:
+#            rowlist.append(value)
+        environment.append(rowlisty)
+            
+
+# plot data
+# matplotlib.pyplot.imshow(environment)
+# matplotlib.pyplot.show()
+
 
 # initialize 
 num_of_agents = 10
-num_of_iterations = 10000
+num_of_iterations = 100000
 agents = []
 
 
 # make the agents
 for i in range(num_of_agents):
-    agents.append([random.randint(0,99),random.randint(0,99)])
+    agents.append(agentframework.Agent(environment))
 
 
 # move the agents in random direction 
+#for j in range(num_of_iterations):
+#    for i in range(num_of_agents):
+#        agents[i].move()
+
+
+# eat
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
-
-        if random.random() < 0.5:
-            agents[i][0] = (agents[i][0] + 1) % 100
-        else:
-            agents[i][0] = (agents[i][0] - 1) % 100
-
-        if random.random() < 0.5:
-            agents[i][1] = (agents[i][1] + 1) % 100
-        else:
-            agents[i][1] = (agents[i][1] - 1) % 100
+        agents[i].move()
+        agents[i].eat()
 
 
 # plot agents on grid
 matplotlib.pyplot.xlim(0, 99)
 matplotlib.pyplot.ylim(0, 99)
+matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
-    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+    matplotlib.pyplot.scatter(agents[i].x, agents[i].y)
 matplotlib.pyplot.show()
 
 
 # distance function
-def distance_bet(agentA, agentB):
-    return (((agentA[0] - agentB[0])**2) + 
-        ((agentA[1] - agentB[1])**2))**0.5
+#def distance_between(agents_row_a, agents_row_b):
+#    return (((agents_row_a.x - agents_row_b.x)**2) + 
+#    ((agents_row_a.y - agents_row_b.y)**2))**0.5
 
-distance = []
+#distance = []
 
 #for i, j in itertools.combinations(agents, 2):
 #    disty = distance_bet(i, j)
 #    distance.append(distance)
 
-for i in range(len(agents)):
-    for j in range(i + 1, len(agents)):
-        disty = distance_bet(agents[i], agents[j])
-        distance.append(disty)
+#for i in range(len(agents)):
+#    for j in range(i + 1, len(agents)):
+#        disty = distance_between(agents[i], agents[j])
+#        distance.append(disty)
 
-end = time.clock()
-print("time = " + str(end - start))
+#end = time.clock()
+#print("time = " + str(end - start))
 
-a = agentframework.Agent()
-print(a.y, a.x)
+# write environment to file
+f2 = open('env.txt', 'w', newline='') 
+writer = csv.writer(f2, delimiter=' ')
+for row in environment:		
+	writer.writerow(row)		# List of values.
+f2.close()
 
 
+# write agent store to file
+for i in agents:
+    print(i.store)
 
-
+f3 = open('stored.txt', 'w', newline='') 
+writer = csv.writer(f3, delimiter=' ')
+for i in agents:
+    listy = []
+    listy.append(i.store)
+    writer.writerow(listy)		# List of values.
+f3.close()
 
 
 
