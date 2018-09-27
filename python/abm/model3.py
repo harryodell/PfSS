29d886f2561348c2c90673e7f36ea043a9591f01
Harry Odell <harry.odell93@gmail.com>
2018-09-27 18:24:18 +0100

    added stopping condition

--- python/abm/model3.py
+++ python/abm/model3.py
@@ -1,12 +1,9 @@
 import random
 import operator
 import matplotlib.pyplot 
-import itertools
-import time
 import agentframework
-import csv 
-import pandas as pd
 import matplotlib.animation 
+# %matplotlib qt
 
 # read in data 
 environment = []
@@ -21,31 +18,37 @@ with open('in.txt', 'r') as file_for_reading:
 
 # initialize 
 num_of_agents = 10
-num_of_iterations = 10
+num_of_iterations = 10000
 neighbourhood = 20
 agents = []
 leny = len(environment[1])
 lenx = len(environment)
+carry_on = True
  
 # make the agents using list comp
 agents = [agentframework.Agent(environment, agents, neighbourhood) for n in range(num_of_agents)]
 
 fig = matplotlib.pyplot.figure(figsize=(7, 7))
 ax = fig.add_axes([0, 0, 1, 1])
+totalenv = sum(sum(x) for x in environment)
 
 def update(blah):
     
-    fig.clear()   
-    
+    fig.clear()  
+    global carry_on
 
     for j in range(num_of_iterations):
         random.shuffle(agents)
         for agent in agents:
             agent.move()
             agent.eat()
-            agent.sick()
+            # agent.sick()
             agent.share_with_neighbours(neighbourhood)
             
+    if sum(sum(x) for x in environment) < totalenv*0.98:
+        carry_on = False
+        print("Stopping condition met - Environent depleted")
+        
         
     # plot agents on grid
     matplotlib.pyplot.xlim(0, lenx)
@@ -54,11 +57,19 @@ def update(blah):
     for i in range(num_of_agents):
         matplotlib.pyplot.scatter(agents[i].x, agents[i].y, s=50)
 
-
+def gen_function(b = [0]):
+    a = 0
+    global carry_on #Not actually needed as we're not assigning, but clearer
+    while (a < num_of_iterations) & (carry_on):
+        yield a			# Returns control and waits next call.
+        a += 1
+    else:
+        print("Stopping condition met - Max iterations reached")
+        
 
 #animation = matplotlib.animation.FuncAnimation(fig, update, interval=1)
-#animation = matplotlib.animation.FuncAnimation(fig, update, repeat=False, frames=num_of_iterations)
-animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=num_of_iterations) 
+#animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=num_of_iterations) 
+animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
 matplotlib.pyplot.show()
 
 
